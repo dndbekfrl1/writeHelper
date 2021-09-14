da7ea050b967607f87f612705d379a7aabd7f34c
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-09-14 16:17:23 +0900

    timer

--- writehelper/src/components/TextBox.js
+++ writehelper/src/components/TextBox.js
@@ -1,35 +1,114 @@
+import { current } from "immer";
 import react, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-let timer;
-function Timeout(from, to) {
-  let current = from;
-  clearTimeout(timer);
-  timer = setTimeout(function tick() {
-    if (current < to) {
-      console.log("tick", current);
+// function Timeout(from, to) {
+//   let current = from;
+//   timer = setTimeout(function tick() {
+//     if (current < to) {
+//       console.log("tick", timer, current);
+//       setTimeout(tick, 1000);
+//     }
+//     current += 1;
+//     if (current === to) {
+//       console.log("time out");
+//     }
+//   }, 1000);
+//   clearTimeout(timer);
+// }
+
+// let timer = {
+//   remind: function (aMessage) {
+//     alert(aMessage);
+//     this.timeoutID = undefined;
+//   },
+
+//   tick: function (current, to) {
+//     console.log("tick", current);
+//     this.ticktok = setTimeout(function tick() {
+//       if (current < to) {
+//         setTimeout(this.tick, 1000);
+//       }
+//       current += 1;
+//     }, 1000);
+//   },
+
+//   setup: function (from, to) {
+//     if (typeof this.timeoutID === "number") {
+//       this.cancel();
+//     }
+//     let current = from;
+//     this.timeoutID = setTimeout(
+//       function (current) {
+//         this.tick(current, to);
+//       }.bind(this),
+//       3000,
+//       current
+//     );
+//   },
+
+//   cancel: function () {
+//     clearTimeout(this.timeoutID);
+//   },
+// };
+let to = 3;
+let cur = 0;
+function setUpTimer() {
+  let timerId = setTimeout(function tick() {
+    if (cur < to) {
+      console.log(timerId, cur, to);
       setTimeout(tick, 1000);
     }
-    current += 1;
+    cur += 1;
+    if (cur === to) {
+      console.log("time out");
+    }
   }, 1000);
+  // clearTimeout(timerId);
+  // console.log("timerId", timerId);
 }
 
+let timerId;
 function TextBox() {
   const [text, setText] = useState(null);
   useEffect(() => {
-    if (text) {
-      Timeout(0, 3);
-    }
+    const setTimer = () => {
+      if (typeof timerId === "number") {
+        console.log("new input", "curID:", timerId);
+        cur = 0;
+        clearTimeout(timerId);
+        timerId = undefined;
+      }
+      timerId = setTimeout(function tick() {
+        if (cur < to) {
+          if (typeof timerId === "number") {
+            console.log(timerId, cur, to);
+            timerId = setTimeout(tick, 1000);
+          }
+        }
+        cur += 1;
+        if (cur === to) {
+          console.log(timerId, "time out");
+        }
+      }, 1000);
+    };
+
+    const timer = setTimeout(() => {
+      setTimer();
+    });
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [text]);
 
   const onChange = (e) => {
-    console.log(e.target.value);
     setText(e.target.value);
   };
 
   return (
     <>
-      <TextAreaBlock onChange={onChange} onKeyUp={null}></TextAreaBlock>
+      <TextAreaBlock onChange={onChange}></TextAreaBlock>
     </>
   );
 }
