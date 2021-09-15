19ac124e89578cb00f55dc08300f6b0c07294772
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-09-15 18:21:43 +0900

    clear textbox

--- writehelper/src/components/TextBox.js
+++ writehelper/src/components/TextBox.js
@@ -1,76 +1,14 @@
 import { current } from "immer";
-import react, { useEffect, useState } from "react";
+import react, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-// function Timeout(from, to) {
-//   let current = from;
-//   timer = setTimeout(function tick() {
-//     if (current < to) {
-//       console.log("tick", timer, current);
-//       setTimeout(tick, 1000);
-//     }
-//     current += 1;
-//     if (current === to) {
-//       console.log("time out");
-//     }
-//   }, 1000);
-//   clearTimeout(timer);
-// }
-
-// let timer = {
-//   remind: function (aMessage) {
-//     alert(aMessage);
-//     this.timeoutID = undefined;
-//   },
-
-//   tick: function (current, to) {
-//     console.log("tick", current);
-//     this.ticktok = setTimeout(function tick() {
-//       if (current < to) {
-//         setTimeout(this.tick, 1000);
-//       }
-//       current += 1;
-//     }, 1000);
-//   },
-
-//   setup: function (from, to) {
-//     if (typeof this.timeoutID === "number") {
-//       this.cancel();
-//     }
-//     let current = from;
-//     this.timeoutID = setTimeout(
-//       function (current) {
-//         this.tick(current, to);
-//       }.bind(this),
-//       3000,
-//       current
-//     );
-//   },
-
-//   cancel: function () {
-//     clearTimeout(this.timeoutID);
-//   },
-// };
-let to = 3;
+let to = 2;
 let cur = 0;
-function setUpTimer() {
-  let timerId = setTimeout(function tick() {
-    if (cur < to) {
-      console.log(timerId, cur, to);
-      setTimeout(tick, 1000);
-    }
-    cur += 1;
-    if (cur === to) {
-      console.log("time out");
-    }
-  }, 1000);
-  // clearTimeout(timerId);
-  // console.log("timerId", timerId);
-}
-
 let timerId;
+
 function TextBox() {
   const [text, setText] = useState(null);
+  const textarea = useRef();
   useEffect(() => {
     const setTimer = () => {
       if (typeof timerId === "number") {
@@ -89,6 +27,9 @@ function TextBox() {
         cur += 1;
         if (cur === to) {
           console.log(timerId, "time out");
+          setText(null);
+          console.log("text", text);
+          textarea.current.value = "";
         }
       }, 1000);
     };
@@ -108,7 +49,11 @@ function TextBox() {
 
   return (
     <>
-      <TextAreaBlock onChange={onChange}></TextAreaBlock>
+      <TextAreaBlock
+        ref={textarea}
+        text={text}
+        onChange={onChange}
+      ></TextAreaBlock>
     </>
   );
 }
